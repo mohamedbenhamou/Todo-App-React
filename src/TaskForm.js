3f5d9c9d85da1b6cdf08591364ab43a970b2a480
mohameddbenhamou <mohamedbenhamou.info@gmail.com>
2023-08-09 11:27:53 +0100

    finishing the projet

--- src/TaskForm.js
+++ src/TaskForm.js
@@ -1,9 +1,15 @@
 import React, { useState } from 'react';
 
-export default function TaskForm() {
+export default function TaskForm({onAdd}) {
     const [taskNane,setTaskName] = useState('');
+    const handleSubmit = (ev) => {
+        ev.preventDefault();
+        onAdd(taskNane);
+        setTaskName('');
+
+    }
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <button>+</button>
       <input type="text" placeholder="Your next task" value={taskNane} onChange={(ev)=>setTaskName(ev.target.value)} />
     </form>
